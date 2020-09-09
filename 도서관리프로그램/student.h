2ed77e5cbf89383df2344baf6814f14a6aea2edc
yujeong <dbwjd8752@naver.com>
2020-09-09 17:23:42 +0900

    student header file 생성

---
+++ 도서관리프로그램/student.h
@@ -0,0 +1,15 @@
+typedef struct stNode{
+	struct stNode* next;
+	char stnum[20];
+	char passward[20];
+	char name[20];
+}student;
+
+student* head;
+student* tail;
+student* member;
+
+void signUp();
+void logIn();
+void Freedata();
+void node_print();
