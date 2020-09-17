9af92aba5f3fa2197adfa5e59ba2a37229c32f89
yujeong <dbwjd8752@naver.com>
2020-09-17 14:30:52 +0900

    book 한글깨짐 변경

--- 도서관리프로그램/book.c
+++ 도서관리프로그램/book.c
@@ -11,31 +11,31 @@
 
 
 
-void Login(){           //�α���â �Լ�
+void Login(){           //로그인창 함수
 
 	char Adminnum[]="admin";
 	char Adminpassword[]="admin";
 	char Num[20];
 	char Password[20];
 
-	printf("\n[�α���]\n");
-	printf("�й�:");
+	printf("\n[로그인]\n");
+	printf("학번:");
 	scanf("%s",Num);
-	printf("���й�ȣ:");
+	printf("비밀번호:");
 	scanf("%s",Password);
 
 	if(strcmp(Adminnum,Num)==0&&strcmp(Adminpassword,Password)==0){
 		Admin();
 	}else{
-		//�л� �α��� �������� Ȯ��...
-		printf("�α��ο� �����߽��ϴ�.\n");
+		//학생 로그인 가능한지 확인...
+		printf("로그인에 실패했습니다.\n");
 	}
 
 }
 
 
 /*
-typedef struct Book{      //���� ���� ��ũ�帮��Ʈ
+typedef struct Book{      //도서 목록 링크드리스트
 	char Book_name[Book_name_size];
 	char Book_company[Book_company_size];
 	char Book_ISBN[Book_ISBN_size];
@@ -44,15 +44,13 @@ typedef struct Book{      //���� ���� ��ũ�帮��Ʈ
 	struct Book *next;
 
 }Book;
-
 Book *Book_head;
-Book *Book_tmp; //�� �� å ��ġ
+Book *Book_tmp; //맨 끝 책 위치
 Book *ttmp;
 Book *ttmp_back;
-
 */
 
-void bk_init(){                          // book ����Ʈ �� ó���κ� ������ �Լ�
+void bk_init(){                          // book 리스트 맨 처음부분 만드는 함수
 
 FILE* fp=fopen("bk.txt","w");
 	Book_head=(Book *)malloc(sizeof(Book));
@@ -64,17 +62,17 @@ FILE* fp=fopen("bk.txt","w");
 
 
 
-int Admin_mode(){                      //������ ���� ���� �Լ� ,���� �� ����
+int Admin_mode(){                      //관리자 모드 출력 함수 ,선택 수 리턴
 	int choice;
-	printf("\n[������ ����]\n");
-	printf("1. ���� ����\n");
-	printf("2. ���� ����\n");
-	printf("3. ���� �뿩\n");
-	printf("4. ���� �ݳ�\n");
-	printf("5. ���� �˻�\n");
-	printf("6. ȸ�� ����\n");
-	printf("7. �α׾ƿ�\n");
-	printf("8.���α׷� ����\n");
+	printf("\n[관리자 모드]\n");
+	printf("1. 도서 등록\n");
+	printf("2. 도서 삭제\n");
+	printf("3. 도서 대여\n");
+	printf("4. 도서 반납\n");
+	printf("5. 도서 검색\n");
+	printf("6. 회원 목록\n");
+	printf("7. 로그아웃\n");
+	printf("8.프로그램 종료\n");
 
 	scanf("%d",&choice);
 	return choice;
@@ -82,29 +80,29 @@ int Admin_mode(){                      //������ ���� ���
 
 
 
-void add_book(){                 //���� ���� �Լ�
+void add_book(){                 //도서 등록 함수
 	Book *p=(Book *)malloc(sizeof(Book));
 
-	printf("[ ���� ���� ]\n");
-	printf("������:");
+	printf("[ 도서 등록 ]\n");
+	printf("도서명:");
 	getchar();
 	fgets(p->Book_name,Book_name_size,stdin);
 	Erase_enter(p->Book_name);
-	printf("���ǻ�:");
+	printf("출판사:");
 	fgets(p->Book_company,Book_name_size,stdin);
 	Erase_enter(p->Book_company);
-	printf("���ڸ�:");
+	printf("저자명:");
 	fgets(p->Book_author,Book_name_size,stdin);
 	Erase_enter(p->Book_author);
 	printf("ISBN:");
 	fgets(p->Book_ISBN,Book_name_size,stdin);
 	Erase_enter(p->Book_ISBN);
 
-	printf("���Ⱑ�� ����(y/n):");
+	printf("대출가능 여부(y/n):");
 	scanf("%c",&p->Book_borrow);
 	getchar();
 	while(p->Book_borrow!='y'&&p->Book_borrow!='n'){
-		printf("'y'�� 'n'�� �ϳ��� �Է��� �ֽʽÿ� :");
+		printf("'y'와 'n'중 하나를 입력해 주십시오 :");
 		scanf("%c",&p->Book_borrow);
 		getchar();
 
@@ -120,9 +118,9 @@ void add_book(){                 //���� ���� �Լ�
 	*/
 }
 
-void remove_book(){              // ���� ���� �Լ�
+void remove_book(){              // 도서 삭제 함수
 	char name[Book_name_size];
-	printf("������:");
+	printf("도서명:");
 	scanf("%s",name);
 	ttmp=Book_head->next;
 	ttmp_back=Book_head;
@@ -131,15 +129,15 @@ void remove_book(){              // ���� ���� �Լ�
 		ttmp=ttmp->next;
 	}
 	if(ttmp==NULL){
-		printf("%s�� ������ ã�� �� �����ϴ�.\n",name);
+		printf("%s란 도서를 찾을 수 없습니다.\n",name);
 	}else{
 		if(ttmp->Book_borrow=='n'){
-			printf("�������� å�� ������ �Ұ����մϴ�.\n");
+			printf("대출중인 책은 삭제가 불가능합니다.\n");
 		}else{
 
 			ttmp_back->next=ttmp->next;
 			free(ttmp);
-			printf("������ �Ϸ��Ǿ����ϴ�.\n");
+			printf("삭제가 완료되었습니다.\n");
 			Load_list_to_file();
 		}
 
@@ -147,30 +145,30 @@ void remove_book(){              // ���� ���� �Լ�
 	}
 }
 
-void borrow_book(){        //���� �뿩 �Լ�
+void borrow_book(){        //도서 대여 함수
 char student[10];
 char ISBN[Book_ISBN_size];
-	printf("[�����뿩]\n");
-	printf("�й� :");
+	printf("[도서대여]\n");
+	printf("학번 :");
 
 	scanf("%s",student);
 	printf("ISBN :");
 	 scanf("%s",ISBN);
-	 //�л� ����Ʈ �̿��ؼ�...(�й��� �´� �л� ������ ��������(�л� ���Ͽ� å�� �߰����ش�)
+	 //학생 리스트 이용해서...(학번에 맞는 학생 있으면 대출해줌(학생 목록에 책도 추가해준다)
 	 ttmp=Book_head->next;
 	 while(ttmp!=NULL&&strcmp(ttmp->Book_ISBN,ISBN)!=0){
 		 ttmp=ttmp->next;
 	 }
 	 if(ttmp==NULL){
-	 	printf("�������� �ʴ� �����Դϴ�.\n") ;
+	 	printf("존재하지 않는 도서입니다.\n") ;
 	 }else{
 
 	 	if(strcmp(ttmp->Book_ISBN,ISBN)==0){
 	 		if(ttmp->Book_borrow=='n'){
-	 			printf("�̹� ������ �����Դϴ�.\n");
+	 			printf("이미 대출된 도서입니다.\n");
 
 			 }else{
-			 	printf("'%s'å�� �����Ǿ����ϴ�.\n",ttmp->Book_name);
+			 	printf("'%s'책이 대출되었습니다.\n",ttmp->Book_name);
 			 	ttmp->Book_borrow='n';
 			 	Load_list_to_file();
 
@@ -181,7 +179,7 @@ char ISBN[Book_ISBN_size];
 
 }
 
-Book* ISBN_cmp(char ISBN[Book_ISBN_size]){  //isbn�� �޾� �����帮��Ʈ �ȿ��� �����ؼ� book���� �ּ� ��ȯ�ϴ� �Լ�
+Book* ISBN_cmp(char ISBN[Book_ISBN_size]){  //isbn을 받아 링스드리스트 안에서 비교해서 book노드 주소 반환하는 함수
 
 	ttmp=Book_head->next;
 
@@ -191,7 +189,7 @@ Book* ISBN_cmp(char ISBN[Book_ISBN_size]){  //isbn�� �޾� �����
 	return ttmp;
 }
 
-void return_book(){      //���� �ݳ� �Լ�
+void return_book(){      //도서 반납 함수
 
 	char ISBN[Book_ISBN_size];
 	printf("ISBN:");
@@ -200,15 +198,15 @@ void return_book(){      //���� �ݳ� �Լ�
 	q=ISBN_cmp(ISBN);
 
 	if(q==NULL){
-	 	printf("�������� �ʴ� �����Դϴ�.\n") ;
+	 	printf("존재하지 않는 도서입니다.\n") ;
 	}else{
 
 	 	if(strcmp(q->Book_ISBN,ISBN)==0){
 	 		if(ttmp->Book_borrow=='y'){
-	 			printf("�̹� �ݳ��� �����Դϴ�.\n");
+	 			printf("이미 반납된 도서입니다.\n");
 
 			 }else{
-			 	printf("'%s'å�� �ݳ��Ǿ����ϴ�.\n",q->Book_name);
+			 	printf("'%s'책이 반납되었습니다.\n",q->Book_name);
 			 	q->Book_borrow='y';
 			 	Load_list_to_file();
 
@@ -219,7 +217,7 @@ void return_book(){      //���� �ݳ� �Լ�
 
 }
 
-void Load_list_to_file(){   //����Ʈ���� ���Ϸ� �Է����ִ� �Լ�
+void Load_list_to_file(){   //리스트에서 파일로 입력해주는 함수
 	FILE* fp= fopen("bk.txt","w");
 	ttmp=Book_head->next;
 	while(ttmp!=NULL){
@@ -230,7 +228,7 @@ void Load_list_to_file(){   //����Ʈ���� ���Ϸ� �Է�
 }
 
 
-void Book_load(){                  //���Ͽ��� ��ũ�帮��Ʈ�� ������ �Լ�->���� ���غ�
+void Book_load(){                  //파일에서 링크드리스트로 만드는 함수->실행 안해봄
 	char name[Book_name_size];
 	char company[Book_company_size];
 	char ISBN[Book_ISBN_size];
@@ -268,18 +266,18 @@ void Book_load(){                  //���Ͽ��� ��ũ�帮��Ʈ
 }
 
 
-int Find_book(){        //[�����˻�]â  ,��ȣ ����
+int Find_book(){        //[도서검색]창  ,번호 리턴
 
 	int num;
 	while(1){
 
-	printf("\n[�����˻�]\n");
-	printf("1. ������ �˻�\n");
-	printf("2. ���ǻ� �˻�\n");
-	printf("3. ISBN �˻�\n");
-	printf("4. ���ڸ� �˻�\n");
-	printf("5. ��ü �˻�\n");
-	printf("6. ���� �޴�\n");
+	printf("\n[도서검색]\n");
+	printf("1. 도서명 검색\n");
+	printf("2. 출판사 검색\n");
+	printf("3. ISBN 검색\n");
+	printf("4. 저자명 검색\n");
+	printf("5. 전체 검색\n");
+	printf("6. 이전 메뉴\n");
 	scanf("%d",&num);
 
 	if(num==1){
@@ -294,7 +292,7 @@ int Find_book(){        //[�����˻�]â  ,��ȣ ����
 		q=ISBN_cmp(isbn);
 
 		if(q==NULL){
-			printf("�������� �ʴ� �����Դϴ�.\n");
+			printf("존재하지 않는 도서입니다.\n");
 		}else{
 			Book_information(q);
 		}
@@ -306,15 +304,15 @@ int Find_book(){        //[�����˻�]â  ,��ȣ ����
 	}else if(num==6){
 		break;
 	}else{
-		printf("1~6������ ���� �Է��� �ֽʽÿ�\n");
+		printf("1~6사이의 수를 입력해 주십시오\n");
 	}
 	}
 }
 
-void Search_book_author(){   //���ڸ� ã���ִ� �Լ�
+void Search_book_author(){   //저자명 찾아주는 함수
 
 	char author[Book_author_size];
-	printf("���ڸ� :");
+	printf("저자명 :");
 	getchar();
 	fgets(author,Book_author_size,stdin);
 	Erase_enter(author);
@@ -332,16 +330,16 @@ void Search_book_author(){   //���ڸ� ã���ִ� �Լ�
 	}
 
 	if(nu==0){
-		printf("���ڸ��� %s�� ������ ã�� �� �����ϴ�.\n",author);
+		printf("저자명이 %s인 도서를 찾을 수 없습니다.\n",author);
 	}else{
-		printf("\n�� %d���� å�� �˻��Ǿ����ϴ�.\n",nu);
+		printf("\n총 %d권의 책이 검색되었습니다.\n",nu);
 	}
 }
 
-void Search_book_company(){        //���ǻ� ã�� �Լ�
+void Search_book_company(){        //출판사 찾는 함수
 
 	char company[Book_company_size];
-	printf("���ǻ��� :");
+	printf("출판사명 :");
 	getchar();
 	fgets(company,Book_company_size,stdin);
 	Erase_enter(company);
@@ -359,30 +357,30 @@ void Search_book_company(){        //���ǻ� ã�� �Լ�
 	}
 
 	if(nu==0){
-		printf("���ǻ簡  %s�� ������ ã�� �� �����ϴ�.\n",company);
+		printf("출판사가  %s인 도서를 찾을 수 없습니다.\n",company);
 	}else{
-		printf("\n�� %d���� å�� �˻��Ǿ����ϴ�.\n",nu);
+		printf("\n총 %d권의 책이 검색되었습니다.\n",nu);
 	}
 }
 
-void Book_information(Book *node){      //���� �ּҸ� �޾� ������ �� �������ִ� �Լ�
+void Book_information(Book *node){      //노드 주소를 받아 정보를 다 출력해주는 함수
 
-	printf("\n������ : %s\n",node->Book_name);
-	printf("���ǻ� : %s\n",node->Book_company);
+	printf("\n도서명 : %s\n",node->Book_name);
+	printf("출판사 : %s\n",node->Book_company);
 	printf("ISBN : %s\n",node->Book_ISBN);
-	printf("���ڸ� : %s\n",node->Book_author);
-	printf("���⿩�� : %c\n",node->Book_borrow);
+	printf("저자명 : %s\n",node->Book_author);
+	printf("대출여부 : %c\n",node->Book_borrow);
 
 }
 
 
-void Erase_enter(char *s){  //���๮�� �����ִ� �Լ�
+void Erase_enter(char *s){  //개행문자 지워주는 함수
 	s[strlen(s)-1]='\0';
 }
 
-void Search_book_name(){         //������ �˻� �Լ�
+void Search_book_name(){         //도서명 검색 함수
 	char name[Book_name_size];
-	printf("å�̸�: ");
+	printf("책이름: ");
 	getchar();
 	fgets(name,Book_name_size,stdin);
 	Erase_enter(name);
@@ -398,14 +396,14 @@ void Search_book_name(){         //������ �˻� �Լ�
 		ttmp=ttmp->next;
 	}
 	if(nu==0){
-		printf("%s�� ������ ã�� �� �����ϴ�.\n",name);
+		printf("%s란 도서를 찾을 수 없습니다.\n",name);
 	}else{
-		printf("�� %d���� å�� �˻��Ǿ����ϴ�.\n",nu);
+		printf("총 %d권의 책이 검색되었습니다.\n",nu);
 	}
 
 }
 
-int Find_word(char *a,char *b){       //�ܾ� ã�� �Լ�
+int Find_word(char *a,char *b){       //단어 찾기 함수
 
 	if(strstr(b,a)==NULL){
 		return 0;
@@ -424,12 +422,12 @@ void books(){
 	}
 
 }
-void Admin(){     //�����ڸ���
+void Admin(){     //관리자모드
 
 		while(1){
 
 	int c_num=0;
-	c_num=Admin_mode();           ///���� �Է½� ���Ѽ��� ����...?
+	c_num=Admin_mode();           ///문자 입력시 무한수열 이유...?
 
 	if(c_num==1){
 		add_book();
@@ -445,14 +443,14 @@ void Admin(){     //�����ڸ���
 		Find_book();
 
 	}else if(c_num==6){
-		//ȿ���� �л� ���Ḯ��Ʈ �ҷ�����
+		//효원이 학생 연결리스트 불러오기
 
 	}else if(c_num==7){
-	 //�� ó�� [������ ������]�� �̵�
+	 //맨 처음 [도서관 서비스]로 이동
 	}else if(c_num==8){
 		break;
 	}else{
-	  printf("1���� 8������ �� �� �ϳ��� �Է��� �ֽʽÿ�.\n");
+	  printf("1부터 8까지의 수 중 하나를 입력해 주십시오.\n");
 	  fflush(stdin);
 	}
 
@@ -466,8 +464,6 @@ int main(){
 
 	bk_init();
 	Login();
-
-
 	return 0;
 }
 */
