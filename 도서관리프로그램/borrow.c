2e526c3aec26e6f3edcdfe40afa83c39d15034d2
LeeMinJii <julia7450@naver.com>
2020-09-17 14:07:36 +0900

    borrow.c수정

--- 도서관리프로그램/borrow.c
+++ 도서관리프로그램/borrow.c
@@ -26,8 +26,8 @@ int confirm_stnum(char *st_num ){  // 존재하는 학번인지 확인하는 함
 	cf_node=head;
 
 	while(1){
-		 if( cf_node->next  == NULL){
-             printf("존재하지 않는 학번입니다.");
+		 if( cf_node  == NULL){
+             printf("존재하지 않는 학번입니다.\n");
              return 3;
              }
 
@@ -39,21 +39,19 @@ int confirm_stnum(char *st_num ){  // 존재하는 학번인지 확인하는 함
 	return 0;
 }
 
-int confirm_isbn(){  //존재하는 isbn인지 확인하는 함수
-	borrow* cf_node;
-	br_tail->next=cf_node;
-	Book* cf_isbn;
-	Book_tmp->next= cf_isbn;
+int confirm_isbn(char *isbn){  //존재하는 isbn인지 확인하는 함수
+	Book* cf_node;
+	cf_node=Book_head;
+	
 	while(1){
-		if(cf_node->isbn == cf_isbn->Book_ISBN )
-			break;
-		Book_tmp->next=cf_isbn->next;
-		cf_isbn=Book_tmp->next;
-		Book_tmp=cf_isbn;
-		if( Book_tmp==NULL){
-			printf("존재하지 않는 isbn 입니다.");
+		if(cf_node==NULL){
+			printf("존재하지 않는 ISBN 입니다.\n");
 			return 4;
 		}
+
+		if(!strcmp(cf_node->Book_ISBN,isbn ))
+			break;
+		cf_node=cf_node->next;
 	}
 	return 0;
 }
@@ -75,88 +73,97 @@ void add_borrowlist(){ //도서대여 연결리스트
 	int b;
 	printf("  ISBN: ");
 	scanf("%s", new_node->isbn);	
-	b=confirm_isbn();
+	b=confirm_isbn(new_node->isbn);
 	if (b==4)
 		return;
 }
 
 void save_borrow(){  //대출정보 저장
 	FILE* brfp=fopen("borrow.txt","w");
+	borrow *tmp=br_head->next;
 
 	while(1){  //borrow연결리스트에 있는 정보탐색
-		borrow *tmp=(borrow*)malloc(sizeof(borrow));
-		br_tail->next=tmp;
-		if (tail==NULL)
-			break;
 		fprintf(brfp, "%s %s\n", tmp->num, tmp->isbn);
-		br_tail->next=tmp->next;
-		tmp=br_tail->next;
-		br_tail=tmp;
+		if (tmp==NULL)
+			break;
+		tmp=tmp->next;
 	}
 	fclose(brfp);
 }
 
 void delete_borrow(){	//도서반납->대출연결리스트에서 삭제
 	borrow* node=(borrow *)malloc(sizeof(borrow));
-	borrow* delnode=br_tail->next;
+	borrow* delnode=br_head;
+	borrow* backnode=delnode;
 	int a,b;
 	printf("학번 : ");
 	scanf("%s",node->num);
-	a=confirm_stnum();  //존재하는 학번인지 확인
+	a=confirm_stnum(node->num);  //존재하는 학번인지 확인
 	if (a==3)
 		return;
 
 	printf("ISBN : ");
 	scanf("%s", node->isbn);
-	b=confirm_isbn();   //존재하는 isbn인지 확인
+	b=confirm_isbn(node->isbn);   //존재하는 isbn인지 확인
 	if (b==4)
 		return;
 	
 	while(1){  //삭제할 노드 탐색
-	if(strcmp(node->num,delnode->num) == 0 && strcmp(node->isbn, delnode->isbn)==0){
-		br_tail=delnode->next;//노드삭제
-		free(delnode);
-		printf("반납되었습니다.");
-		break;
-	}
-	else if (br_tail->next==NULL){
-		printf("대출 정보를 찾을 수 없습니다.");
-		break;
-	}
-	else{
-		br_tail->next=delnode->next;
-        delnode=br_tail->next;
-        br_tail=delnode;
-	}
-	}
-}
-
-/*
-char find_mylist(){  //학번으로 내 대여목록 찾은 후 isbn반환
-	int stnum;
-	scanf("%d",stnum);
-	borrow *my_list=(borrow *)malloc(sizeof(borrow));
-
-	while (br_tail->next == NULL){
-
-	if (stnum == my_list->num){
-		printf("%s\n",my_list.isbn);//책 정보 출력-도서연결리스트
-		my_list=br_tail->next;
-		br_tail=my_list;
-	}
-	else if (stnum != my_list.num){
-		my_list=br_tail->next;
-		br_tail=my_list;
+		if(delnode->next==NULL){
+			printf("대출 정보를 찾을 수 없습니다.");
+            return 1;
 		}
+		if(strcmp(node->num,delnode->num) == 0 && strcmp(node->isbn, delnode->isbn)==0){
+			backnode=delnode->next;//노드삭제
+			free(delnode);
+			printf("반납되었습니다.");
+			return 0;
+		}
+		backnode=delnode;
+	    delnode=delnode->next;
 	}
 }
-*/
 
-int main() {
+void Admin(){     //관리자모드
+ 
+         while(1){
+
+     int c_num=0;
+     c_num=Admin_mode();           ///문자 입력시 무한수열 이유...? 
+ 
+     if(c_num==1){
+         add_book();
+         Load_list_to_file();
+     }else if(c_num==2){
+         remove_book();
+ 
+     }else if(c_num==3){
+         add_borrowlist();
+     }else if(c_num==4){
+         delete_borrow();
+     }else if(c_num==5){
+         Find_book();
+ 
+     }else if(c_num==6){
+         //효원이 학생 연결리스트 불러오기 
+ 
+     }else if(c_num==7){
+      //맨 처음 [도서관 서비스]로 이동
+     }else if(c_num==8){
+         break;
+     }else{
+       printf("1부터 8까지의 수 중 하나를 입력해 주십시오.\n");
+       fflush(stdin);
+     }
+
+	
+	int main() {
 	borrow_init();
+	st_InitNode();
 //	confirm_stnum;
 //	confirm_isbn;
-	add_borrowlist();
-	save_borrow();
-	delete_borrow();
-}
+//	add_borrowlist();
+//	save_borrow();
+//	delete_borrow();
+	Admin();
+	}
